6899c217dba1bb8c5a5a8751914a70ddfe6c97b8
Jake <jake123ps4@gmail.com>
2024-01-11 23:31:45 +0100

    files added

--- javascripts/gamesbuttons.js
+++ javascripts/gamesbuttons.js
@@ -1,7 +1,36 @@
 $(window).on("load", function loadfunction() {
     var pokerbutton = $("#pokertrymebutton")
+    var baccaratbutton = $("#baccarattrymebutton")
+    var blackjackbutton = $("#blackjacktrymebutton")
+    var crapsbutton = $("#crapstrymebutton")
+    var kenobutton = $("#kenotrymebutton")
+    var roulettebutton = $("#roulettetrymebutton")
+    var slotsbutton = $("#slotstrymebutton")
+    var videopokerbutton = $("#videopokertrymebutton")
 
     pokerbutton.click(function pokerbtn() {
         window.open("https://www.wikihow.com/Play-Poker")
     }) 
+    baccaratbutton.click(function pokerbtn() {
+        window.open("https://www.wikihow.com/Play-Baccarat")
+    }) 
+    blackjackbutton.click(function pokerbtn() {
+        window.open("https://www.wikihow.com/Play-Blackjack")
+    }) 
+    crapsbutton.click(function pokerbtn() {
+        window.open("https://www.wikihow.com/Play-Craps")
+    }) 
+    kenobutton.click(function pokerbtn() {
+        window.open("https://www.wikihow.com/Play-Keno")
+    }) 
+    roulettebutton.click(function pokerbtn() {
+        window.open("https://www.wikihow.com/Play-Roulette")
+    }) 
+    slotsbutton.click(function pokerbtn() {
+        window.open("https://www.wikihow.com/Play-Slot-Machines")
+    }) 
+    videopokerbutton.click(function pokerbtn() {
+        window.open("https://www.wikihow.com/Win-at-Video-Poker")
+    }) 
 })
+
